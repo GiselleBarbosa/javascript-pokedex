56307c81e8292510ff64d837f565bf720555db5e
GiselleBarbosa <g88barbosa@gmail.com>
2023-09-12 07:59:28 -0300

    feat: API request added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log('rodou');
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+// forma declarativa - funcções anônimas
+fetch(url)
+	.then(function (response) {
+		response.json();
+	})
+	.then(function (jsonBody) {
+		console.log(jsonBody);
+	})
+	.catch(function (error) {
+		console.error(error);
+	})
+	.finally(function () {
+		console.log("Requisição finalizada");
+	});
