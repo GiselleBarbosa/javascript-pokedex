31b87a670a1b8280b31c473f903c63c38767152e
GiselleBarbosa <g88barbosa@gmail.com>
2023-09-12 08:01:45 -0300

    refac:  arrow function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,9 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-// forma declarativa - funcções anônimas
+// ARROW FUNCTION
 fetch(url)
-	.then(function (response) {
-		response.json();
-	})
-	.then(function (jsonBody) {
-		console.log(jsonBody);
-	})
-	.catch(function (error) {
-		console.error(error);
-	})
-	.finally(function () {
-		console.log("Requisição finalizada");
-	});
+	.then(response => response.json()
+  .then(jsonBody => console.log("jsonBody =>", jsonBody)))
+	.catch(error => console.error(error))
+	.finally(() => console.log("Requisição finalizada"));
