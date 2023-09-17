57aa6deb4e2e1d47d2befe38c3c43165705ecaeb
GiselleBarbosa <g88barbosa@gmail.com>
2023-09-17 14:35:17 -0300

    refac: created class and added pokeon images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,14 @@
 function convertPokemonToLi(pokemon) {
 	return `<li class="pokemon">
-  <span class="number">#001</span>
+  <span class="number">#${pokemon.number}</span>
   <span class="name">${pokemon.name}</span>
 
   <div class="detail">
     <ol class="types">
-      <li class="type">grass</li>
-      <li class="type">poison</li>
-    </ol>
-
+    ${pokemon.types.map((type)=> `<li class="type">${type}</li>`).join('')}
+     </ol>
     <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      src="${pokemon.photo}"
       alt="${pokemon.name}" />
   </div>
 </li>`;
@@ -19,5 +17,7 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-	pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join("");
+	const newHtml = pokemons.map(convertPokemonToLi).join("");
+
+	pokemonList.innerHTML = newHtml;
 });
